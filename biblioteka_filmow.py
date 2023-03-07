c600c9dae1628d5b4961063cd926ed2941e14f47
Mateusz Cholewa <mcholewa828@gmail.com>
2023-03-07 21:58:57 +0100

    Add biblioteka_flimow.py

---
+++ biblioteka_filmow.py
@@ -0,0 +1,23 @@
+from faker import Faker
+fake = Faker()
+
+class Movie_Library:
+    def __init__(self, title, year, type, number_of_plays):
+        self.title = title
+        self.year = year
+        self.type = type
+        self.views = 0
+
+    def play(self, view = 1):
+        self.views += view
+
+
+class Series_Library(Movie_Library):
+    def __init__(self, episode_number, season_number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.episode_numer = episode_number
+        self.season_number = season_number
+
+    def play(self, view = 1):
+        self.views += view
+        
