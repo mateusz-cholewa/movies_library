8982c91ed02322ede7409b341cf13e2f5a153083
Mateusz Cholewa <mcholewa828@gmail.com>
2023-03-08 20:53:23 +0100

    Add get_movies and get_series

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -2,7 +2,7 @@ from faker import Faker
 fake = Faker()
 
 class Movie_Library:
-    def __init__(self, title, year, type, number_of_plays):
+    def __init__(self, title, year, type):
         self.title = title
         self.year = year
         self.type = type
@@ -10,6 +10,9 @@ class Movie_Library:
 
     def play(self, view = 1):
         self.views += view
+
+    def __str__(self):
+        return f"Oglądasz {self.title} z roku {self.year}"
 
 
 class Series_Library(Movie_Library):
@@ -21,4 +24,32 @@ class Series_Library(Movie_Library):
     def play(self, view = 1):
         self.views += view
 
-print(fake.word().capitalize())
+    def __str__(self):
+        return f"Oglądasz {self.title} S{self.season_number:02d}E{self.episode_numer:02d}"
+
+library = []
+
+for _ in range(5):
+    films = Movie_Library(title=fake.word().capitalize(), year=fake.year(), type=fake.word().capitalize())
+    library.append(films)
+    series = Series_Library(title=fake.word().capitalize(), year=fake.year(), type=fake.word().capitalize(), episode_number=fake.random_int(1,20), season_number=fake.random_digit())
+    library.append(series)
+
+    def get_movies():
+        movies = []
+        for obj in library:
+            if season_number not in library:
+                movies.append(obj)
+        return sorted(movies)
+
+    def get_series():
+        series = []
+        for obj in library:
+            if season_number > 0:
+                series.append(obj)
+        return sorted(series)
+
+print(series.get_series())
+    
+
+
